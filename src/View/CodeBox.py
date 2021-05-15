5cb50876baa06ee1d8a83c679f02f8b3ab55bb91
Member <feher.janos.zoltan@gmail.com>
2021-05-15 21:27:50 +0200

    Added comment tinting.

--- src/View/CodeBox.py
+++ src/View/CodeBox.py
@@ -24,9 +24,12 @@ class CodeBox:
                           fg=self.__loader.colorPalettes.getColor("boxFontNormal"))
 
         self.getFont()
+        self.tintingThreads=[]
+
         self.box.pack(side=BOTTOM, anchor=S, fill=BOTH)
         self.loadCode()
         self.__loader.currentEditor = self.box
+        self.addTinting()
 
         self.stopThread = False
         self.__loader.stopThreads.append(self)
@@ -34,7 +37,6 @@ class CodeBox:
         t = Thread(target=self.checker)
         t.daemon = True
         t.start()
-
 
         self.box.bind("<Key>", self.keyPressed)
         self.box.bind("<KeyRelease>", self.keyReleased)
@@ -59,10 +61,14 @@ class CodeBox:
         from time import sleep
         while (self.__loader.mainWindow.dead == False and self.stopThread==False):
             if (self.keyPress==True):
-                sleep(0.5)
+                sleep(0.35)
                 if (self.keyPress == False):
                     self.__loader.BFG9000.saveAllCode()
+                    self.addTinting()
 
+            for t in self.tintingThreads:
+                if t.is_alive() == False:
+                    self.tintingThreads.remove(t)
 
             sleep(0.05)
 
@@ -86,10 +92,48 @@ class CodeBox:
 
     def mouseWheel(self, event):
         if self.ctrlPressed == True:
-            if event.delta > 0 and int(self.__config.getValueByKey("codeBoxFont"))<48:
+            if event.delta > 0 and int(self.__config.getValueByKey("codeBoxFont"))<36:
                 self.__config.setKey("codeBoxFont", str(int(self.__config.getValueByKey("codeBoxFont"))+1))
                 self.getFont()
 
             if event.delta < 0 and int(self.__config.getValueByKey("codeBoxFont"))>12:
                 self.__config.setKey("codeBoxFont", str(int(self.__config.getValueByKey("codeBoxFont"))-1))
                 self.getFont()
+
+    def addTinting(self):
+        text = self.box.get(0.0, END).split("\n")
+        for tag in self.box.tag_names():
+            self.box.tag_remove(tag, "0.0", END)
+        self.addCommentTinting(text)
+
+    def addCommentTinting(self, text):
+        self.box.tag_config("comment", foreground=self.__loader.colorPalettes.getColor("comment"),
+                                  font=self.__loader.fontManager.getFont(round(self.__fontSize), False, True, False))
+        lineNum = 0
+        for line in text:
+            lineNum+=1
+            q = Thread(target=self.commentTintingThread, args=[line, lineNum])
+            q.daemon = True
+            self.tintingThreads.append(q)
+            q.start()
+
+    def commentTintingThread(self, line, lineNum):
+        if line.startswith("#") or line.startswith("*"):
+            self.box.tag_add("comment", f"{str(lineNum)}.0", f"{str(lineNum)}.{str(len(line))}")
+        else:
+            poz = self.getPositionOfFirstDeliminator(line)
+            if poz != None:
+                self.box.tag_add("comment", f"{str(lineNum)}.{str(poz)}", f"{str(lineNum)}.{str(len(line))}")
+
+    def getPositionOfFirstDeliminator(self, line):
+        deliminator = self.__config.getValueByKey("deliminator")
+        valid = 0
+        for position in range(0, len(line) - len(deliminator)):
+            if line[position] == "(":
+                valid += 1
+            elif line[position] == ")":
+                valid -= 1
+            elif valid == 0:
+                if line[position:position + len(deliminator)] == deliminator:
+                    return(position)
+        return(None)
