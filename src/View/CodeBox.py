53a00fc95fcbee7bf128bd9c0d74c6aa594b9edb
Member <feher.janos.zoltan@gmail.com>
2021-05-15 20:46:26 +0200

    Added mouse wheel support to codeboxes.

--- src/View/CodeBox.py
+++ src/View/CodeBox.py
@@ -8,6 +8,7 @@ class CodeBox:
 
         self.__loader = loader
         self.keyPress = False
+        self.ctrlPressed = False
 
         self.__bank = self.__loader.BFG9000.getSelected()[0]
         self.__section = self.__loader.BFG9000.getSelected()[1]
@@ -37,7 +38,7 @@ class CodeBox:
 
         self.box.bind("<Key>", self.keyPressed)
         self.box.bind("<KeyRelease>", self.keyReleased)
-
+        self.box.bind("<MouseWheel>", self.mouseWheel)
 
 
     def getFont(self):
@@ -52,11 +53,11 @@ class CodeBox:
     def loadCode(self):
         self.box.delete(1.0, END)
         self.box.insert(1.0, self.__loader.virtualMemory.codes[self.__bank][self.__section].code)
+        self.lastLen=len(self.__loader.virtualMemory.codes[self.__bank][self.__section].code)
 
     def checker(self):
         from time import sleep
         while (self.__loader.mainWindow.dead == False and self.stopThread==False):
-
             if (self.keyPress==True):
                 sleep(0.5)
                 if (self.keyPress == False):
@@ -67,7 +68,28 @@ class CodeBox:
 
     def keyPressed(self, event):
         self.keyPress = True
-        self.__editor.changed = True
+        try:
+            test = ord(event.keysym)
+            self.__editor.changed = True
+        except:
+            if len(self.__loader.virtualMemory.codes[self.__bank][self.__section].code)!=self.lastLen:
+                self.__editor.changed = True
+                self.lastLen = len(self.__loader.virtualMemory.codes[self.__bank][self.__section].code)
+
+        if (event.keysym == "Control_L" or event.keysym == "Control_R"):
+            self.ctrlPressed = True
 
     def keyReleased(self, event):
         self.keyPress = False
+        if (event.keysym == "Control_L" or event.keysym == "Control_R"):
+            self.ctrlPressed = False
+
+    def mouseWheel(self, event):
+        if self.ctrlPressed == True:
+            if event.delta > 0 and int(self.__config.getValueByKey("codeBoxFont"))<48:
+                self.__config.setKey("codeBoxFont", str(int(self.__config.getValueByKey("codeBoxFont"))+1))
+                self.getFont()
+
+            if event.delta < 0 and int(self.__config.getValueByKey("codeBoxFont"))>12:
+                self.__config.setKey("codeBoxFont", str(int(self.__config.getValueByKey("codeBoxFont"))-1))
+                self.getFont()
